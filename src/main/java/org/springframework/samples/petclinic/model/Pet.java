98ea6e5793f1634d450016ce1472792b93c76bab
Edwin Bosveld <ebosveld@users.noreply.github.com>
2020-10-06 16:14:10 +0200

    Making a breaking change to show failing pipeline

--- src/main/java/org/springframework/samples/petclinic/model/Pet.java
+++ src/main/java/org/springframework/samples/petclinic/model/Pet.java
@@ -58,7 +58,7 @@ public class Pet extends NamedEntity {
     @JoinColumn(name = "owner_id")
     private Owner owner;
 
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet", fetch = FetchType.EAGER)
+    @OneToMany(cascade = CascadeType, mappedBy = "pet", fetch = FetchType.EAGER)
     private Set<Visit> visits;
 
 
